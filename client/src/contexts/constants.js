908e142ac58c54cd860664048f3446ef373dda3e
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2023-12-17 21:50:35 +0100

    Fix Netlify routing

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "somedeployURL"
+    : "https://venerable-licorice-89c550.netlify.app"
 
 export const LOCAL_STORAGE_TOKEN_NAME = "harilearn-mern"
 
