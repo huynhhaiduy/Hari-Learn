4db6ab42af9ca45b60acbe18d57762dddf5b3f66
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2023-12-18 01:32:26 +0100

    change url

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
   cors({
-    origin: process.env.NODE_ENV === 'production' ? 'https://venerable-licorice-89c550.netlify.app' : '*',
+    origin: process.env.NODE_ENV === 'production' ? 'https://luxury-kashata-6b6a6c.netlify.app' : '*',
     optionsSuccessStatus: 200,
     methods: 'GET,PUT,POST,DELETE',
   })
