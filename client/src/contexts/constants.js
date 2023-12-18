8e67e486fc9320777c522c17fff9c3fb5af6cefb
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2023-12-18 02:22:32 +0100

    fix

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "https://hari-learn.onrender.com"
+    : "somedeployURL"
 
 export const LOCAL_STORAGE_TOKEN_NAME = "harilearn-mern"
 
--- server/index.js
+++ server/index.js
@@ -1,20 +1,39 @@
-require('dotenv').config();
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const authRouter = require('./routes/auth');
-const postRouter = require('./routes/post');
-const { options } = require('./routes/auth');
+const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.devkxas.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      }
+    );
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
 
 connectDB();
+
 const app = express();
 app.use(express.json());
-// app.use(cors());
-app.use(
-  cors({
-    origin: process.env.NODE_ENV === 'production' ? 'https://luxury-kashata-6b6a6c.netlify.app' : '*',
-    optionsSuccessStatus: 200,
-    methods: 'GET,PUT,POST,DELETE',
-  })
-);
+app.use(cors());
+
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
